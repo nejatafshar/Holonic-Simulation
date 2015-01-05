08ec74ec36b695e9e63f8b274937d620c1b892f8
Nejat <nejatafshar@mailinator.com>
2015-01-04 16:25:32 -0800

    simulation added to ui, application name and organization name is added

--- Holonic_Simulation/Main_App/main.cpp
+++ Holonic_Simulation/Main_App/main.cpp
@@ -4,6 +4,11 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+
+    qApp->setOrganizationName("Home");
+    qApp->setApplicationName("Holonic_Simulation");
+
     MainWindow w;
     w.showMaximized();
 
