06d3d3326a1f2c0862e7aa3a0ac070c4b89347cb
Nejat <nejatafshar@mailinator.com>
2015-02-04 16:41:29 -0800

    Change primary settings

--- Holonic_Simulation/Main_App/main.cpp
+++ Holonic_Simulation/Main_App/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
 
     a.setStyle("Fusion");
 
-    qApp->setOrganizationName("Home");
+    qApp->setOrganizationName("University");
     qApp->setApplicationName("Holonic_Simulation");
 
     MainWindow w;
